20091378e237fe152b97d1ca8a5d17c6ff9701bc
Ati <190020009@iitdh.ac.in>
2022-02-02 17:29:02 +0530

    Text to Speech Synthesis Completed

--- src/controllers/APICallController.ts
+++ src/controllers/APICallController.ts
@@ -4,9 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 import textToSpeech from '@google-cloud/text-to-speech';
 import { writeFile } from 'fs';
 import { promisify } from 'util';
+import readXlsxFile from 'read-excel-file/node';
 
-// Creates a client
 const client = new textToSpeech.TextToSpeechClient();
+
 export class APICallController {
   static async convertToSpeech(
     req: Request,
@@ -14,24 +15,63 @@ export class APICallController {
     next: NextFunction
   ) {
     try {
-      const text = 'JOHN WILLIAMSON LATHAM IS THE CAPTAIN OF NEW ZEALAND.';
+      readXlsxFile('./src/Assets/CMU-Arctic_translation_EN-HN.xlsx').then(
+        async (rows) => {
+          // console.log(rows);
+          // Construct the request
+          for (let i = 1; i < rows.length; i++) {
+            const request: any = {
+              input: { text: rows[i][1] },
+              // Select the language and SSML voice gender (optional)
+              voice: { languageCode: 'en-IN', name: 'en-IN-Wavenet-A' },
+              // select the type of audio encoding
+              audioConfig: {
+                audioEncoding: 'LINEAR16',
+                pitch: 0,
+                speakingRate: 1
+              }
+            };
+            // Performs the text-to-speech request
 
-      // Construct the request
-      const request: any = {
-        input: { text: text },
-        // Select the language and SSML voice gender (optional)
-        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-        // select the type of audio encoding
-        audioConfig: { audioEncoding: 'MP3' }
-      };
+            const [response]: any = await client.synthesizeSpeech(request);
+            // Write the binary audio content to a local file
+            const writeFileCustom = promisify(writeFile);
+            console.log(response);
+            await writeFileCustom(
+              './English/' + rows[i][0] + '.wav',
+              response.audioContent,
+              'binary'
+            );
 
-      // Performs the text-to-speech request
-      const [response]: any = await client.synthesizeSpeech(request);
-      // Write the binary audio content to a local file
-      const writeFileCustom = promisify(writeFile);
-      await writeFileCustom('output.mp3', response.audioContent, 'binary');
-      console.log('Audio content written to file: output.mp3');
+            const request2: any = {
+              input: { text: rows[i][2] },
+              // Select the language and SSML voice gender (optional)
+              voice: { languageCode: 'hi-IN', name: 'hi-IN-Wavenet-A' },
+              // select the type of audio encoding
+              audioConfig: {
+                audioEncoding: 'LINEAR16',
+                pitch: 0,
+                speakingRate: 1
+              }
+            };
+            // Performs the text-to-speech request
+
+            const [response2]: any = await client.synthesizeSpeech(request2);
+            // Write the binary audio content to a local file
+            console.log(response2);
+            await writeFileCustom(
+              './Hindi/' + rows[i][0] + '.wav',
+              response2.audioContent,
+              'binary'
+            );
+            console.log('files of current row have been created');
+          }
+          // `rows` is an array of rows
+          // each row being an array of cells.
+        }
+      );
     } catch (err) {
+      console.log(err);
       next(err);
     }
   }
